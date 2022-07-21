a87e01a1503d77e9fe11f518b2a046657fb4f293
hamiltonsjr <hamiltonsjr@hotmail.com>
2022-07-21 19:50:16 -0300

    commit

---
+++ payment/src/main/java/br/com/food/payment/PaymentApplication.java
@@ -0,0 +1,14 @@
+package br.com.food.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// teste
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
